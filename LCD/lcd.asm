b2ddba6c5df64d9b46e818757c499ca7d7e1a99e
Nathan Stark <nathan.stark@syncbak.com>
2020-09-08 10:20:02 -0500

    finished untested lcd code, we'll see how it works later

--- LCD/lcd.asm
+++ LCD/lcd.asm
@@ -1,19 +1,68 @@
 .include "m328pdef.inc"
 
-.def charReg   = r0  ; register that holds the current working character
-.def modeReg   = r16 ; register that controls the states of the pins (input/output)
-.def stateReg  = r17 ; register that controls whether the pins are high/low
+.def charReg    = r18  ; register that holds the current working character
+.def dataReg    = r16 ; register that controls the 8 data lines to the LCD (PORTD)
+.def selectReg  = r17 ; register that controls the R/W and RS lines (PORTB)
 
 ; initialize stack pointer
-ldi r16, LOW(RAMSTART)
+ldi r16, LOW(RAMEND)
 out SPL, r16
-ldi r16, HIGH(RAMSTART)
+ldi r16, HIGH(RAMEND)
 out SPH, r16
 
 ; this program uses PORTD pins 0-7 to write to the LCD
 ; they are used as outputs so configure them as such
-clr r16
 ldi r16, $FF  ; 1 = output so set all the pins to outputs
 out DDRD, r16
 
-start:
+; this sets the PORTB pins as well
+ldi r16, $0C
+out DDRB, r16
+
+init_display:
+    ldi selectReg, $00 ; all of the control lines are 0 for this
+    ldi dataReg, $38   ; this command sets the display to 2 line mode with a 5-8 character font
+    
+    out PORTB, selectReg ; write the command to the LCD
+    out PORTD, dataReg
+
+    ldi dataReg, $0E ; turn on the display and cursor, control lines are unchanged so we don't need to modify the register
+
+    out PORTD, dataReg
+
+    ldi dataReg, $06 ; sets the display to move the character right after each character is written
+
+    out PORTD, dataReg
+
+    ldi charReg, $48 ; capital H
+    rjmp write_char
+
+    ldi charReg, $65 ; lower case e
+    rjmp write_char
+
+    ldi charReg, $6C ; lower case l
+    rjmp write_char
+
+    ldi charReg, $6C ; lower case l
+    rjmp write_char
+
+    ldi charReg, $6F ; lower case o
+    rjmp write_char
+
+infinite_loop:
+    nop
+    rjmp infinite_loop
+
+write_char:
+    ldi selectReg, $08   ; set RS to 1 and keep R/W at 0
+    mov dataReg, charReg ; copy the character to be written to the output register
+
+    out DDRB, selectReg
+    out DDRD, dataReg
+
+    ldi r23, $01
+    rjmp delay10ms
+
+    ret
+
+.include "delay.inc"
