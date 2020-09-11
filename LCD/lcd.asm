ba881a965bf347bac081629122e051eb26b35c67
Nathan Stark <nathan.stark@syncbak.com>
2020-09-11 13:12:04 -0500

    Might have found a potential fix to the init issues, still needs testing

--- LCD/lcd.asm
+++ LCD/lcd.asm
@@ -1,8 +1,8 @@
 .include "m328pdef.inc"
 
-.def charReg    = r18  ; register that holds the current working character
-.def dataReg    = r16 ; register that controls the 8 data lines to the LCD (PORTD)
-.def selectReg  = r17 ; register that controls the R/W and RS lines (PORTB)
+.def charReg      = r18 ; register that holds the current working character
+.def dataReg      = r16 ; register that controls the 8 data lines to the LCD (PORTD)
+.def selectReg    = r17 ; register that controls the R/W and RS lines (PORTB)
 
 ; initialize stack pointer
 ldi r16, LOW(RAMEND)
@@ -20,38 +20,32 @@ ldi r16, $0C
 out DDRB, r16
 
 init_display:
-    ldi r23, 10
+    ldi r23, 12      ; delay for 120ms to allow the display to turn on
     rcall delay10ms
 
-    ldi selectReg, $00 ; all of the control lines are 0 for this
-    ldi dataReg, $38   ; this command sets the display to 2 line mode with a 5-8 character font
-    
-    out PORTB, selectReg ; write the command to the LCD
-    out PORTD, dataReg
+    ldi dataReg, $30 ; the next three commands tell the display to reset manually so we can initialize
+    rcall write_cmd
 
-    ldi r23, 1
-    rcall delay10ms
+    ldi dataReg, $30
+    rcall write_cmd
 
-    ldi dataReg, $0E ; turn on the display and cursor, control lines are unchanged so we don't need to modify the register
+    ldi dataReg, $30
+    rcall write_cmd
 
-    out PORTD, dataReg
+    ldi dataReg, $38 ; this command sets the display to 2 line mode with a 5-8 character font
+    rcall write_cmd
 
-    ldi r23, 1
-    rcall delay10ms
+    ldi dataReg, $08 ; turn off the display and cursor
+    rcall write_cmd
 
-    ldi dataReg, $06 ; sets the display to move the character right after each character is written
+    ldi dataReg, $01 ; clears the display
+    rcall write_cmd
 
-    out PORTD, dataReg
+    ldi dataReg, $06 ; entry mode set
+    rcall write_cmd
 
-    ldi r23, 1
-    rcall delay10ms
-
-    ldi dataReg, $01 ; should clear the display
-
-    out PORTD, dataReg
-
-    ldi r23, 1
-    rcall delay10ms
+    ldi dataReg, $0C ; three steps ago we left the display off, so turn it back on
+    rcall write_cmd
 
     ldi charReg, $48 ; capital H
     rcall write_char
@@ -71,6 +65,17 @@ init_display:
 infinite_loop:
     nop
     rjmp infinite_loop
+
+write_cmd:
+    ldi selectReg $00
+    
+    out DDRB, selectReg
+    out DDRD, dataReg
+
+    ldi r23, 1
+    rcall delay10ms
+
+    ret
 
 write_char:
     ldi selectReg, $08   ; set RS to 1 and keep R/W at 0
